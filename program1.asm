da0146be2dc101c3c653f9da5f56af7e742a5cc1
troygob <lorenzo.ragasa20@gmail.com>
2026-05-19 10:01:06 +0800

    modified:   program1.asm

--- program1.asm
+++ program1.asm
@@ -5,6 +5,8 @@ default rel
 section .bss
 ;--- constants --- 
 section .data
+string_literal_0: db "not equal", 0
+string_literal_1: db "equal", 0
 ;--- Entry Point --- 
 section .text
 global main
@@ -27,13 +29,17 @@ main:
 	CMP qword [rsp], 0
 	JE L1
 ; -- PRINT ---
-; NOT IMPLEMENTED 
+	LEA rcx, [rel string_literal_0]
+	XOR eax, eax
+	CALL printf
 ; -- HALT ---
 	JMP EXIT_LABEL
  -- LabeL ---
 L1:
 ; -- PRINT ---
-; NOT IMPLEMENTED 
+	LEA rcx, [rel string_literal_1]
+	XOR eax, eax
+	CALL printf
 ; -- HALT ---
 	JMP EXIT_LABEL
 EXIT_LABEL:
